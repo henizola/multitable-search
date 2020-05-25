3d5a85d9d94c4157bfb760a5fd2536e78306df54
henizola <z.w.henok@gmail.com>
2020-05-25 18:00:10 +0300

    integreted the spinner withthe app

--- src/App.js
+++ src/App.js
@@ -1,50 +1,47 @@
-import React from "react";
-import "./App.css";
-import List from "./components/list/list.component";
-import Search from "./components/search/search.component";
-import { SearchProvider } from "./context/context.context";
+import React, { useState, useEffect } from 'react';
+import './App.css';
+import Search from './components/search/search.component';
+import { SearchProvider } from './context/context.context';
+import WithSpinner from './components/with-spinner/with-spiner.component';
 
-class App extends React.Component {
-  constructor() {
-    super();
+const App = () => {
+	const [countries, setCountries] = useState([]);
+	const [loading, setLoading] = useState(true);
 
-    this.state = {
-      countries: [],
-    };
-  }
+	useEffect(() => {
+		const func = async () => {
+			const datas = await fetch(
+				'https://raw.githubusercontent.com/mledoze/countries/master/countries.json'
+			);
+			const count = await datas.json();
 
-  componentDidMount = async () => {
-    const datas = await fetch(
-      "https://raw.githubusercontent.com/mledoze/countries/master/countries.json"
-    );
-    const count = await datas.json();
+			for (let i = 0; i < 250; i++) {
+				let country = {
+					name: count[i].name.official,
+					capital: count[i].capital[0],
+					region: count[i].region,
+					subRegion: count[i].subregion,
+					latitude: count[i].latlng[0].toFixed(2),
+					longrude: count[i].latlng[1].toFixed(2),
+				};
+				const newCount = [...countries, country];
+				setCountries(newCount);
+				console.log(newCount);
+			}
+			setLoading(false);
+		};
+		func();
+	}, []);
 
-    for (let i = 0; i < 250; i++) {
-      let country = {
-        name: count[i].name.official,
-        capital: count[i].capital[0],
-        region: count[i].region,
-        subRegion: count[i].subregion,
-        latitude: count[i].latlng[0].toFixed(2),
-        longrude: count[i].latlng[1].toFixed(2),
-      };
-      const newCount = [...this.state.countries, country];
-      this.setState({ countries: newCount });
-    }
-  };
-
-  render() {
-    const { ...otherProps } = this.state;
-    return (
-      <div className="App">
-        <h1>Country/Capital Data Multi-Search Service</h1>
-        <SearchProvider>
-          <Search />
-          <List {...otherProps} />
-        </SearchProvider>
-      </div>
-    );
-  }
-}
+	return (
+		<div className='App'>
+			<h1>Country/Capital Data Multi-Search Service</h1>
+			<SearchProvider>
+				<Search />
+				<WithSpinner countries={countries} isLoading={loading} />;
+			</SearchProvider>
+		</div>
+	);
+};
 
 export default App;
